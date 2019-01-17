12c66b8974c73d671916bd201f04d438d19e18c5
trwiley <taylor.r.wiley@gmail.com>
2019-01-16 19:26:45 -0500

    Removed JS codepen code. Added in HTML as component.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <window></window>
   </div>
 </template>
 
 <script>
-
+import Window from './components/Window'
 export default {
   name: 'app',
   components: {
+    'window': Window
   }
 }
 </script>
