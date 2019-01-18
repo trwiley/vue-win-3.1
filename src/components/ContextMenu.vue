0701512eca0bc545b5aa58664320727fd0f8fa02
trwiley <taylor.r.wiley@gmail.com>
2019-01-17 20:29:17 -0500

    WIP: making the CSS more SASS-y

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -3,11 +3,6 @@
      <div v-for="(txt, index) in menu" :key="index">
          <context-menu :msg="txt"></context-menu>
      </div>
-    <!-- <div class="context-item"><span style="text-decoration:underline;">R</span>estore</div>
-    <div class="context-item"><span style="text-decoration:underline;">M</span>ove</div>
-    <div class="context-item"><span style="text-decoration:underline;">S</span>ize</div>
-    <div class="context-item">Mi<span style="text-decoration:underline;">n</span>imize</div>
-    <div class="context-item">Ma<span style="text-decoration:underline;">x</span>imize</div> -->
 </div>   
 </template>
 <script>
