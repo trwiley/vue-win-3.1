d98731305358a6f1e47b7d7e91e874664eb11a25
trwiley <taylor.r.wiley@gmail.com>
2019-01-30 19:14:31 -0500

    Updated readme.

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Partly "just because" but also to act as a starting point for a modern Vue app t
 
 ## Credits
 
-Thanks to Matthijs Lanser for creating the Windows 3.1 GUI CSS styling which I used as a starting point for this.
+Thanks to Matthijs Lanser for creating the [Windows 3.1 GUI CSS styling](https://codepen.io/m-lanser/pen/wzYjJQ) which I used as a starting point for this.
---
+++ src/components/Win31.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="win31">
+    <drag-it-dude>
+      <window :menuitems="menuitems" :icons="icondata" height="500px" width="500px"></window>
+    </drag-it-dude>
+  </div>
+</template>
+
+<script>
+import WinWindow from './components/Window'
+import DragItDude from '../node_modules/vue-drag-it-dude/dist/vue-drag-it-dude.esm'
+export default {
+  name: 'app',
+  components: {
+    'window': WinWindow,
+    'drag-it-dude': DragItDude
+  },
+  data () {
+    return {
+      menuitems: [],
+      icondata: [
+        {
+          'image': 'http://windowsfreak.de/icons/win3.11/folder.png',
+          'caption': 'Accessories'
+        },
+        {
+          'image': 'http://windowsfreak.de/icons/win3.11/folder.png',
+          'caption': 'Games'
+        },
+        {
+          'image': 'http://windowsfreak.de/icons/win3.11/folder.png',
+          'caption': 'Applications'
+        },
+        {
+          'image': 'http://windowsfreak.de/icons/win3.11/folder.png',
+          'caption': 'main'
+        }
+      ],
+    }
+  },
+  created () {
+    this.populateMenuItems()
+  },
+  methods: {
+    populateMenuItems () {
+      this.menuitems = ['File', 'Option', 'Windows', 'Help']
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
